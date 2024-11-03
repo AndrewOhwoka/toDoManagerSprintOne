2f00b7f501b105f62337c04da87c45959aa27deb
Andrew Ohwoka <150045330+AndrewOhwoka@users.noreply.github.com>
2024-11-03 09:30:19 -0330

    UserCommit

---
+++ User.java
@@ -0,0 +1,31 @@
+public class User {
+    private String name;
+    private TaskList taskList;
+
+    public User(String name) {
+        this.name = name;
+        this.taskList = new TaskList();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public TaskList getTaskList() {
+        return taskList;
+    }
+
+    public void addTask(String description) {
+        Task newTask = new Task(description);
+        taskList.addTask(newTask);
+    }
+
+    public void markTaskAsCompleted(String description) {
+        taskList.markTaskAsCompleted(description);
+    }
+
+    public void printTasks() {
+        System.out.println("Tasks for " + name + ":");
+        taskList.printTasks();
+    }
+}
